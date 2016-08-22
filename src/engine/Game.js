a72263c579b8705db18c51c37f785bc0af2ca899
Nicolas <nicolas.merinian@gmail.com>
2016-08-23 00:16:35 +0200

    add stage attr and add.stage method

--- src/engine/Game.js
+++ src/engine/Game.js
@@ -16,6 +16,24 @@ Deer.Game = function Game(containerId, width, height) {
 		this.height = 600;
 	}
 
+	this.stage = null;
+
 	return this;
 
+};
+
+Deer.Game.prototype.add = function add(type, options) {
+
+	if (!type)
+		throw "Deer.Game.add.type undefined";
+
+	if (!options)
+		throw "Deer.Game.add.options undefined";
+
+	switch (type) {
+		case 'stage':
+			this.stage = new Deer.Stage(options);
+			break;
+	}
+	
 };
